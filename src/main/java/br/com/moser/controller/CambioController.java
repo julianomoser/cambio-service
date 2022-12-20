108fd9dc5e3a5256f17c2f135007002094fb77ee
Juliano Moser <juliano.moserjj@gmail.com>
2022-12-20 16:06:23 -0300

    Recuperando a Porta da Instância em Execução

--- src/main/java/br/com/moser/controller/CambioController.java
+++ src/main/java/br/com/moser/controller/CambioController.java
@@ -1,6 +1,9 @@
 package br.com.moser.controller;
 
 import br.com.moser.model.Cambio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.PropertiesPropertySource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,11 +18,15 @@ import java.math.BigDecimal;
 @RequestMapping("cambio-service")
 public class CambioController {
 
+    @Autowired
+    private Environment environment;
+
     @GetMapping("/{amount}/{from}/{to}")
     public Cambio getCambio(@PathVariable("amount") BigDecimal amount,
                             @PathVariable("from") String from,
                             @PathVariable("to") String to) {
+        var port = environment.getProperty("local.server.port");
         return new Cambio(1L, from, to, BigDecimal.ONE,
-                BigDecimal.ONE, "PORT 8000");
+                BigDecimal.ONE, port);
     }
 }
