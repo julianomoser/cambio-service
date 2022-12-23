a75fb67116d904b8843d067e1d840ab88205c302
Juliano Moser <juliano.moserjj@gmail.com>
2022-12-23 09:56:51 -0300

    Conectando ao Zipkin com Zipkin e Sleuth

--- src/main/java/br/com/moser/controller/CambioController.java
+++ src/main/java/br/com/moser/controller/CambioController.java
@@ -4,6 +4,7 @@ import br.com.moser.model.Cambio;
 import br.com.moser.service.CambioService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +18,7 @@ import java.math.RoundingMode;
 /**
  * @author Juliano Moser
  */
+@Slf4j
 @Tag(name = "Cambio endpoint")
 @RestController
 @RequestMapping("cambio-service")
@@ -33,6 +35,8 @@ public class CambioController {
     public Cambio getCambio(@PathVariable("amount") BigDecimal amount,
                             @PathVariable("from") String from,
                             @PathVariable("to") String to) {
+
+        log.info("getCambio is called whit -> {}, {} and {}", amount, from, to);
 
         var cambio = cambioService.findByFromAndTo(from, to);
         var port = environment.getProperty("local.server.port");
